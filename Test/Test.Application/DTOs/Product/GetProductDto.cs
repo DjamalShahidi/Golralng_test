09a80bfbbf4be6aa21778e0ab55b1827a67a2759
DESKTOP-2A5H47P\Jamal <jamalshahidi20@gmail.com>
2024-02-27 12:45:16 +0330

    Cleaning

--- Test/Test.Application/DTOs/Product/GetProductDto.cs
+++ Test/Test.Application/DTOs/Product/GetProductDto.cs
@@ -6,22 +6,7 @@ namespace Test.Application.DTOs.Product
     {
         public string Title { get; set; }
 
-        public string Code { get; set; }
-
-        public string Description { get; set; }
-
-        public string ImgUrl { get; set; }
-
-        public int CategoryId { get; set; }
-
-        public string CategoryTitle { get; set; }
-
-        public List<GetProductDtoFeature> Features { get; set; }=new List<GetProductDtoFeature>();  
+ 
     }
-    record class GetProductDtoFeature
-    {
-        public string Name { get; set; }
 
-        public object Value { get; set; }
-    }
 }
